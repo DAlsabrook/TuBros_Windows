3a256d611f2b0c73a42ec79902a02ab4d7531f17
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-20 14:46:48 -0500

    Commenting out slider file to fix deployment issue with slider not being defined

--- app/slider.js
+++ app/slider.js
@@ -1,36 +1,36 @@
-import React from 'react';
+// import React from 'react';
 // import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import styles from './styles/page.module.css';
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import styles from './styles/page.module.css';
 
-// Carousel images
-const images = [
-  '/window_placeholder-1.jpg',
-  '/window_placeholder-2.png',
-  '/window_placeholder-3.jpeg',
-];
+// // Carousel images
+// const images = [
+//   '/window_placeholder-1.jpg',
+//   '/window_placeholder-2.png',
+//   '/window_placeholder-3.jpeg',
+// ];
 
-export default function Carousel() {
-  // Used for carousel
-  const settings = {
-    dots: true,
-    lazyLoad: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-  };
+// export default function Carousel() {
+//   // Used for carousel
+//   const settings = {
+//     dots: true,
+//     lazyLoad: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 3000,
+//   };
 
-  return (
-    <Slider {...settings}>
-      {images.map((src, index) => (
-        <div key={index} className={styles.carouselImageWrapper}>
-          <img src={src} alt={`Carousel image ${index + 1}`} className={styles.carouselImage} />
-        </div>
-      ))}
-    </Slider>
-  );
-}
+//   return (
+//     <Slider {...settings}>
+//       {images.map((src, index) => (
+//         <div key={index} className={styles.carouselImageWrapper}>
+//           <img src={src} alt={`Carousel image ${index + 1}`} className={styles.carouselImage} />
+//         </div>
+//       ))}
+//     </Slider>
+//   );
+// }
